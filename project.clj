e5438423648fda9dcfd836c1b73354ba91435f01
Plínio Balduino <pbalduino+github@gmail.com>
2015-05-26 12:35:37 -0300

    Updated dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject posture "0.1.2"
+(defproject posture "0.1.3"
   :description "Clojure wrapper for Postmon API"
   :url "https://github.com/PostmonAPI/posture"
   :license {:name "http://opensource.org/licenses/MIT"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/data.json "0.2.4"]])
+  :dependencies [[org.clojure/data.json "0.2.6"]])
